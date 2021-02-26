eccab2b5cdfabcd896824bd76e800730f9e79d18
Kanta HORIO <kanta.horio@gmail.com>
2021-02-26 15:04:18 +0900

    updated new config items

--- firmware-platformio/STEP400_firmware/src/loadConfig.cpp
+++ firmware-platformio/STEP400_firmware/src/loadConfig.cpp
@@ -63,7 +63,6 @@ void loadConfig() {
     // Alarm and Report
     JsonObject alarmAndReport = doc["alarmAndReport"];
     JsonArray alarmAndReport_reportBUSY = alarmAndReport["reportBUSY"];
-    JsonArray alarmAndReport_reportFLAG = alarmAndReport["reportFLAG"];
     JsonArray alarmAndReport_reportHiZ = alarmAndReport["reportHiZ"];
     JsonArray alarmAndReport_reportHomeSwStatus = alarmAndReport["reportHomeSwStatus"];
     JsonArray alarmAndReport_reportDir = alarmAndReport["reportDir"];
@@ -95,18 +94,32 @@ void loadConfig() {
 
     // Driver settings
     JsonObject driverSettings = doc["driverSettings"];
-    JsonArray driverSettings_stepMode = driverSettings["stepMode"];
+    JsonArray driverSettings_homingAtStartup = driverSettings["homingAtStartup"];
+    JsonArray driverSettings_homingDirection = driverSettings["homingDirection"];
+    JsonArray driverSettings_homingSpeed = driverSettings["homingSpeed"];
     JsonArray driverSettings_homeSwMode = driverSettings["homeSwMode"];
+    JsonArray driverSettings_prohibitMotionOnHomeSw = driverSettings["prohibitMotionOnHomeSw"];
     JsonArray driverSettings_limitSwMode = driverSettings["limitSwMode"];
+    JsonArray driverSettings_prohibitMotionOnLimitSw = driverSettings["prohibitMotionOnLimitSw"];
+    JsonArray driverSettings_goUnitlTimeout = driverSettings["goUnitlTimeout"];
+    JsonArray driverSettings_releaseSwTimeout = driverSettings["releaseSwTimeout"];
+    JsonArray driverSettings_stepMode = driverSettings["stepMode"];
     JsonArray driverSettings_isCurrentMode = driverSettings["isCurrentMode"];
     JsonArray driverSettings_slewRate = driverSettings["slewRate"];
     JsonArray driverSettings_electromagnetBrakeEnable = driverSettings["electromagnetBrakeEnable"];
     JsonArray driverSettings_brakeTransitionDuration = driverSettings["brakeTransitionDuration"];
     uint16_t slewRateVal[6] = { SR_114V_us, SR_220V_us, SR_400V_us, SR_520V_us, SR_790V_us, SR_980V_us };
     for (i = 0; i < NUM_OF_MOTOR; i++) {
-        microStepMode[i] = driverSettings_stepMode[i] | STEP_SEL_1_128;
+        bHomingAtStartup[i] = driverSettings_homingAtStartup[i] | false;
+        homingDirection[i] = driverSettings_homingDirection[i] | REV;
+        homingSpeed[i] = driverSettings_homingSpeed[i] | 50.0;
         homeSwMode[i] = driverSettings_homeSwMode[i] | true; // true: SW_USER, false: SW_HARDSTOP
+        bProhibitMotionOnHomeSw[i] = driverSettings_prohibitMotionOnHomeSw[i] | false;
         limitSwMode[i] = driverSettings_limitSwMode[i] | true;
+        bProhibitMotionOnLimitSw[i] = driverSettings_prohibitMotionOnLimitSw[i] | false;
+        goUntilTimeout[i] = driverSettings_goUnitlTimeout[i] | 10000;
+        releaseSwTimeout[i] = driverSettings_releaseSwTimeout[i] | 10000;
+        microStepMode[i] = driverSettings_stepMode[i] | STEP_SEL_1_128;
         isCurrentMode[i] = driverSettings_isCurrentMode[i] | false;
         slewRateNum[i] = constrain((driverSettings_slewRate[i] | 5), 0, 5); // default SR_980V_us
         slewRate[i] = slewRateVal[slewRateNum[i]];
